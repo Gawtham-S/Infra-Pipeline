3e97234a161743591f392cd93cb9104ab95fe43a
Gawtham S <gawthamgabby123@gmail.com>
2026-07-20 09:34:30 +0530

    added QA files

---
+++ environments/qa/main.tf
@@ -0,0 +1,20 @@
+module "vpc" {
+
+    source = "../../modules/vpc"
+    vpc_cidr = "11.0.0.0/16"
+    subnet_cidr = "11.0.1.0/24"
+    subnet_az = "${var.aws_region}a"
+    env = "qa"
+}
+
+
+module "ec2" {
+
+    source = "../../modules/ec2"
+    ami_id = "ami-02d23a03f80ba79fc"
+    instance_type = "t3.micro"
+    subnet_id = module.vpc.subnet_id
+    ec2_count = 2
+    env = "qa"
+
+}
---
+++ environments/qa/variables.tf
@@ -0,0 +1,6 @@
+variable "aws_region" {
+  
+  type = string
+  default = "ap-southeast-1"
+  description = "in which region the resource going to get created"
+}
